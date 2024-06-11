4091f5ba3d0062e7302957d871636dda2883eda9
atn976 <119575129+atn976@users.noreply.github.com>
2024-06-11 21:48:06 +0200

    Update migration.php

--- migration.php
+++ migration.php
@@ -6,25 +6,25 @@ class InitialMigration extends AbstractMigration
 {
     public function up()
     {
-        // Create the database if it does not exist
+        // Créer la base de données si elle n'existe pas
         $this->execute('CREATE DATABASE IF NOT EXISTS expat CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci');
 
-        // Use the created database
+        // Utiliser la base de données créée
         $this->execute('USE expat');
 
-        // Create the article table
+        // Créer la table article
         $this->table('article')
             ->addColumn('title', 'string', ['limit' => 80])
             ->addColumn('content', 'text')
             ->create();
 
-        // Create the category table
+        // Créer la table category
         $this->table('category')
             ->addColumn('name', 'string', ['limit' => 45])
             ->addColumn('description', 'string', ['limit' => 255])
             ->create();
 
-        // Create the article_has_category table
+        // Créer la table article_has_category
         $this->table('article_has_category')
             ->addColumn('article_id', 'integer')
             ->addColumn('category_id', 'integer')
@@ -32,7 +32,7 @@ class InitialMigration extends AbstractMigration
             ->addForeignKey('category_id', 'category', 'id', ['delete'=> 'CASCADE', 'update'=> 'NO_ACTION'])
             ->create();
 
-        // Insert data into category table
+        // Insérer des données dans la table category
         $this->table('category')
             ->insert([
                 ['name' => 'emploi', 'description' => 'Recherche et demande d\'emploi'],
